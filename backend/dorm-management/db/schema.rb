7c4c35bab5fbd4def892efae703bec9348652f44
Tom Gijselinck <tomgijselinck@gmail.com>
2016-06-27 21:16:55 +0200

    Add database index to ensure user email uniqueness at database level

--- backend/dorm-management/db/schema.rb
+++ backend/dorm-management/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160623064256) do
+ActiveRecord::Schema.define(version: 20160627191245) do
 
   create_table "inactive_weeks", force: :cascade do |t|
     t.date     "start"
@@ -27,5 +27,7 @@ ActiveRecord::Schema.define(version: 20160623064256) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
