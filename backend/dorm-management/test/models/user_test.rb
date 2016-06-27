f680e52f765b541606cca5c36b749afaaf912e8a
Tom Gijselinck <tomgijselinck@gmail.com>
2016-06-27 20:22:21 +0200

    Add user name and email validation

--- backend/dorm-management/test/models/user_test.rb
+++ backend/dorm-management/test/models/user_test.rb
@@ -1,7 +1,50 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(name: 'John', email: 'johndoe@example.com')
+  end
+
+  test 'valid user' do
+    assert @user.valid?
+  end
+
+  test 'name should be present' do
+    @user.name = ' '
+    assert_not @user.valid?
+  end
+
+  test 'email should be present' do
+    @user.email = ' '
+    assert_not @user.valid?
+  end
+
+  test 'name should not be to long' do
+    @user.name = 'x' * 51
+    assert_not @user.valid?
+  end
+
+  test 'email should not be to long' do
+    @user.email = 'x' * 244 + '@example.com'
+    assert_not @user.valid?
+  end
+
+  test 'valid email addresses' do
+    valid_addresses = %w[john@example.com JOHN@doe.COM SOME_user@gmail.com
+                         first.last@kuleuven.be a+b@doe.com]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test 'email validation should reject invalid email addresses' do
+    invalid_addresses = %w[john@example,com john_at_doe.com first.last@example.
+                           first.last@ku_leuven.be doe@a+b.com a.b@kul..be]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
 end
