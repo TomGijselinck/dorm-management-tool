f02994c1e0844e221764d86f07292253cf0024dc
Tom Gijselinck <tomgijselinck@gmail.com>
2016-06-27 20:21:07 +0200

    Do migration

--- backend/dorm-management/db/schema.rb
+++ backend/dorm-management/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160623062929) do
+ActiveRecord::Schema.define(version: 20160623064256) do
+
+  create_table "inactive_weeks", force: :cascade do |t|
+    t.date     "start"
+    t.date     "end"
+    t.integer  "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
