e67f2c93bcafccf7c0bb9e37122f2801ec711d4f
Tom Gijselinck <tomgijselinck@gmail.com>
2016-06-28 07:39:09 +0200

    Add email downcasing test

--- backend/dorm-management/test/models/user_test.rb
+++ backend/dorm-management/test/models/user_test.rb
@@ -57,6 +57,13 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
+  test 'email addresses should be saved in lower-case' do
+    mixed_case_email = 'JohnDoe@example.com'
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
   test 'password should be present' do
     @user.password = ' '
     assert_not @user.valid?
