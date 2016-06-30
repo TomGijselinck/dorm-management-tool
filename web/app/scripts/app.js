1b1afd5b696e5d2685518a61b49c4308f0e1267e
Tom Gijselinck <tomgijselinck@gmail.com>
2016-06-30 11:46:25 +0200

    Initialize web interface

---
+++ web/app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name dormManagementToolApp
+ * @description
+ * # dormManagementToolApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('dormManagementToolApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngMessages'
+  ]);
---
+++ web/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dormManagementToolApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dormManagementToolApp
+ */
+angular.module('dormManagementToolApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
