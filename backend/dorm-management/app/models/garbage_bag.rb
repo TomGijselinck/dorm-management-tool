21237c87b032bbe0912d628333d069b18b04e5cc
Tom Gijselinck <tomgijselinck@gmail.com>
2016-07-04 09:01:17 +0200

    Add validation of garbage bags with accompanying tests

--- backend/dorm-management/app/models/garbage_bag.rb
+++ backend/dorm-management/app/models/garbage_bag.rb
@@ -1,3 +1,8 @@
 class GarbageBag < ActiveRecord::Base
+  validates :name, presence: true, length: { maximum: 50}, uniqueness: true
+  validates :status, presence: true
+  validates_presence_of :user, message: 'user should exist'
+  validates_inclusion_of :status, :in => %w( ok full),
+                         message: 'status should be either ok or full'
   belongs_to :user
 end
--- backend/dorm-management/test/models/garbage_bag_test.rb
+++ backend/dorm-management/test/models/garbage_bag_test.rb
@@ -1,7 +1,46 @@
 require 'test_helper'
 
 class GarbageBagTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.create(name: 'John', email: 'johndoe@example.com',
+                        password: 'password', password_confirmation: 'password')
+    @garbage_bag = GarbageBag.new(name: 'Green', status: 'ok',
+                                  user_id: @user.id)
+  end
+  
+  test 'valid garbage bag' do
+    assert @garbage_bag.valid?
+  end
+
+  test 'name should be present' do
+    @garbage_bag.name = ''
+    assert_not @garbage_bag.valid?
+  end
+
+  test 'status should be present' do
+    @garbage_bag.status = ''
+    assert_not @garbage_bag.valid?
+  end
+
+  test 'user id should be of an existing user' do
+    @garbage_bag.user_id = -1
+    assert_not @garbage_bag.valid?
+  end
+
+  test 'status should be either ok or full' do
+    @garbage_bag.status = 'I think it is full'
+    assert_not @garbage_bag.valid?
+  end
+
+  test 'name should not be too long' do
+    @garbage_bag.name = 'x' * 51
+    assert_not @garbage_bag.valid?
+  end
+
+  test 'name should be unique' do
+    duplicate_garbage_bag = @garbage_bag.dup
+    @garbage_bag.save
+    assert_not duplicate_garbage_bag.valid?
+  end
 end
