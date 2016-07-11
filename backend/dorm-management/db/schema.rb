244360b0bf1d77fe3ab4da9606f9965208a19d8a
Tom Gijselinck <tomgijselinck@gmail.com>
2016-07-11 16:20:13 +0200

    Add dorm model

--- backend/dorm-management/db/schema.rb
+++ backend/dorm-management/db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160711094226) do
+ActiveRecord::Schema.define(version: 20160711133023) do
+
+  create_table "dorms", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "garbage_bag_duties", force: :cascade do |t|
     t.datetime "datetime"
