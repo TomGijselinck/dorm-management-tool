81ab9324db9a8409eaada82f34d3d19113a0c429
Tom Gijselinck <tomgijselinck@gmail.com>
2016-07-12 13:02:36 +0200

    Use residents summary in dormservice and update used userservice methods

--- web/app/scripts/services/dormservice.js
+++ web/app/scripts/services/dormservice.js
@@ -8,46 +8,58 @@
  * Service in the dormManagementToolApp.
  */
 angular.module('dormManagementToolApp')
-  .service('DormService', ['$filter', function ($filter) {
+  .service('DormService', ['$filter', '$http', 'UserService',
+    function ($filter, $http, UserService) {
     var mv = this;
-    this.residents = [
-      {
-        "name": "Tom",
-        "id": 1,
-        "garbage_bag_duties": [
-          {
-            "name": "brown",
-            "completed": 5
-          },
-          {
-            "name": "green",
-            "completed": 2
-          },
-          {
-            "name": "blue",
-            "completed": 3
-          }
-        ]
-      },
-      {
-        "name": "Jim",
-        "id": 10,
-        "garbage_bag_duties": [
-          {
-            "name": "brown",
-            "completed": 3
-          },
-          {
-            "name": "green",
-            "completed": 4
-          },
-          {
-            "name": "blue",
-            "completed": 3
-          }
-        ]
-      }
-    ];
+    this.dormId = UserService.getDormId();
+    this.getData = function () {
+      $http({method: 'GET', url: 'http://localhost:3000/dorms/'+mv.dormId+'/residents_summary.json'})
+        .then(function (response) {
+          mv.residents = response.data;
+        },
+        function () {
+          console.log('Failed to GET residents summary');
+          mv.residents = [
+            {
+              "name": "Tom",
+              "id": 1,
+              "garbage_bag_duties": [
+                {
+                  "name": "brown",
+                  "completed": 5
+                },
+                {
+                  "name": "green",
+                  "completed": 2
+                },
+                {
+                  "name": "blue",
+                  "completed": 3
+                }
+              ]
+            },
+            {
+              "name": "Jim",
+              "id": 10,
+              "garbage_bag_duties": [
+                {
+                  "name": "brown",
+                  "completed": 3
+                },
+                {
+                  "name": "green",
+                  "completed": 4
+                },
+                {
+                  "name": "blue",
+                  "completed": 3
+                }
+              ]
+            }
+          ];
+        });
+    };
+    this.getData();
     this.getNextResponsible = function (bag_name, current_responsible_id) {
       var current_resident = $filter('filter')(mv.residents, {id: current_responsible_id})[0];
       var current_duty = $filter('filter')(current_resident.garbage_bag_duties, {name: bag_name})[0];
