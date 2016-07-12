754b21d50252a44ad17011df17835ef56965e010
Tom Gijselinck <tomgijselinck@gmail.com>
2016-07-12 08:53:25 +0200

    Finalize empty trash function

--- web/app/scripts/controllers/garbage.js
+++ web/app/scripts/controllers/garbage.js
@@ -25,6 +25,7 @@ angular.module('dormManagementToolApp')
           console.log('GET garbage_bags.json failed!');
           mv.bags = [
             {
+              "id": 1,
               "name": "purple",
               "status": "full",
               "responsible": {
@@ -33,6 +34,7 @@ angular.module('dormManagementToolApp')
               }
             },
             {
+              "id": 2,
               "name": "yellow",
               "status": "ok",
               "responsible": {
@@ -41,6 +43,7 @@ angular.module('dormManagementToolApp')
               }
             },
             {
+              "id": 3,
               "name": "indigo",
               "status": "full",
               "responsible": {
@@ -61,23 +64,25 @@ angular.module('dormManagementToolApp')
         console.log('failed to set garbage status');
       });
     };
-    this.emptyTrash = function (id, name) {
-      var newResponsible = DormService.getNextResponsible(name);
-      var bag = $filter('filter')(mv.bags, {name: name})[0];
+    this.emptyTrash = function (garbage_id, garbage_name, user_id) {
+      var newResponsible = DormService.getNextResponsible(garbage_name, user_id);
+      var bag = $filter('filter')(mv.bags, {name: garbage_name})[0];
       bag.status = 'ok';
-      bag.responsible = newResponsible.name;
+      bag.responsible.name = newResponsible.name;
+      bag.responsible.id = newResponsible.user_id;
       var body = JSON.stringify(
         {"garbage_bag": {
           "status": "ok",
           "user_id": newResponsible.user_id
           }
         });
-      var url = 'http://localhost:3000/garbage_bags/' + id + '.json';
+      var url = 'http://localhost:3000/garbage_bags/' + garbage_id + '.json';
       $http({method: 'PATCH', url: url, data: body}).then(function (response) {
           //ok!
         },
         function () {
           console.log('failed to empty trash');
         });
-    }
+      GarbageDutyService.addCompletedDuty(user_id, garbage_id, new Date());
+    };
   }]);
