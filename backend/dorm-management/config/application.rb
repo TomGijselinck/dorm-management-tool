dfbb34594b632dae777dfa12e4754c0f2e195fc0
Tom Gijselinck <tomgijselinck@gmail.com>
2016-07-19 22:17:41 +0200

    Use environment variables for local settings

--- backend/dorm-management/config/application.rb
+++ backend/dorm-management/config/application.rb
@@ -39,5 +39,12 @@ module DormManagement
                  :max_age => 0
       end
     end
+
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
   end
 end
