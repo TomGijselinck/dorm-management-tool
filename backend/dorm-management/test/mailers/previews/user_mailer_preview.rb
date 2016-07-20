e3759619d29996e795daae8302b030139e6ecd23
Tom Gijselinck <tomgijselinck@gmail.com>
2016-07-20 07:55:16 +0200

    Add mailer previewer

--- backend/dorm-management/test/mailers/previews/user_mailer_preview.rb
+++ backend/dorm-management/test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
-
+  def new_duty_preview
+    UserMailer.new_duty(GarbageBag.first)
+  end
 end
