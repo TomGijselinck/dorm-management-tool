a6bea54332355e71130977cc4d19f95c06306f0e
Tom Gijselinck <tomgijselinck@gmail.com>
2016-07-25 15:10:38 +0200

    Refactor garbage controller

--- web/app/scripts/controllers/garbage.js
+++ web/app/scripts/controllers/garbage.js
@@ -12,6 +12,8 @@ angular.module('dormManagementToolApp')
     function ($filter, $mdDialog, ApiService, DormService, GarbageDutyService, UserService, HelperService) {
       var mv = this;
 
+      this.user_id = UserService.getId();
+
       this.getData = function () {
         ApiService.getGarbageBags()
           .then(function (response) {
@@ -33,45 +35,27 @@ angular.module('dormManagementToolApp')
         });
       };
 
-      this.tryToEmptyTrash = function (garbage_id, garbage_name, assigned_user_id, status, event) {
+      this.tryToEmptyTrash = function (garbage_bag, event) {
         var user_id = UserService.getId();
-        if (assigned_user_id != user_id) {
-          mv.showConfirmNotAssigned(garbage_id, garbage_name, event);
-        } else if (status =='ok') {
+        if (garbage_bag.responsible.id != user_id) {
+          mv.showConfirmNotAssigned(garbage_bag, event);
+        } else if (garbage_bag.status =='ok') {
         //  this check is not done when not signed in, as it is assumed that if a user
         //  tries to empty a waste bag not assigned to him, he knows if it is not empty
-          mv.showConfirmNotFull(garbage_id, garbage_name, event)
+          mv.showConfirmNotFull(garbage_bag, event)
         } else {
-          this.emptyTrash(garbage_id, garbage_name)
+          this.emptyTrash(garbage_bag)
         }
       };
 
-      this.emptyTrash = function (garbage_id, garbage_name, assigned_user_id) {
+      this.emptyTrash = function (garbage_bag) {
         var user_id = UserService.getId();
-        var newResponsible = DormService.getNextResponsible(garbage_name, user_id);
-        var bag = $filter('filter')(mv.bags, {name: garbage_name})[0];
-        bag.status = 'ok';
-        bag.responsible.name = newResponsible.name;
-        bag.responsible.id = newResponsible.id;
-        var resident = $filter('filter')(DormService.residents, {id: assigned_user_id})[0];
-        resident.number_of_completed_duties++;
-        var body = JSON.stringify(
-          {
-            "garbage_bag": {
-              "status": "ok",
-              "user_id": newResponsible.id
-            }
-          });
-        ApiService.patchGarbageBag(garbage_id, body).then(function () {
-            console.log('successfully updated garbage bag status and responsible');
-          },
-          function () {
-            console.log('failed to empty trash');
-          });
-        GarbageDutyService.addCompletedDuty(user_id, garbage_id, new Date());
+        var newResponsible = DormService.getNextResponsible(garbage_bag.name, user_id);
+        updateGarbageBag(garbage_bag, newResponsible, 'ok');
+        GarbageDutyService.addCompletedDuty(user_id, garbage_bag.id, new Date());
       };
 
-      this.showConfirmNotAssigned = function(garbage_id, garbage_name, event) {
+      this.showConfirmNotAssigned = function(garbage_bag, event) {
         var confirm = $mdDialog.confirm()
           .title('Empty waste bag not assigned to you?')
           .textContent('This will be added to your completed garbage duties.')
@@ -79,13 +63,14 @@ angular.module('dormManagementToolApp')
           .ok('Empty trash')
           .cancel('Cancel');
         $mdDialog.show(confirm).then(function() {
-          mv.emptyTrash(garbage_id, garbage_name);
+          mv.emptyTrash(garbage_bag);
+          console.log('empty trash not assigned')
         }, function() {
           console.log('You decided not to empty the trash.');
         });
       };
 
-      this.showConfirmNotFull = function(garbage_id, garbage_name, event) {
+      this.showConfirmNotFull = function(garbage_bag, event) {
         var confirm = $mdDialog.confirm()
           .title('Empty waste bag which is not full?')
           .textContent('Looks like this garbage bag is not full yet (or it\'s status it not up to date). ' +
@@ -95,9 +80,30 @@ angular.module('dormManagementToolApp')
           .ok('Empty trash')
           .cancel('Cancel');
         $mdDialog.show(confirm).then(function() {
-          mv.emptyTrash(garbage_id, garbage_name);
+          mv.emptyTrash(garbage_bag);
         }, function() {
           console.log('You decided not to empty the trash.');
         });
       };
+
+      function updateGarbageBag(garbage_bag, responsible, status) {
+        garbage_bag.status = status;
+        garbage_bag.responsible = responsible;
+        var resident = $filter('filter')(DormService.residents,
+          {id: garbage_bag.responsible.id})[0];
+        resident.number_of_completed_duties++;
+        var body = JSON.stringify(
+          {
+            "garbage_bag": {
+              "status": status,
+              "user_id": responsible.id
+            }
+          });
+        ApiService.patchGarbageBag(garbage_bag.id, body).then(function () {
+            console.log('successfully updated garbage bag status and responsible');
+          },
+          function () {
+            console.log('failed to update garbage bag');
+          });
+      }
   }]);
